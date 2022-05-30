3199bf6cfb8b7b85c4389928983cddeb3da8031f
Sebsaurus <seby.emami@gmail.com>
2022-05-29 18:22:44 -0700

    commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# CSI_6_ART
-This repo is the codebase for the LSBU CSI_6_ART (AR/VR) module. 
--- test.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-
-x=list(range(100)) 
-y=[i**2 for i in x]
-
-plt.plot(x,y)
-
-plt.show()
